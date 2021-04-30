986faaa8c50d52bcb7aa4eee48249e9d51c437a0
auliyanurul <58121850+auliyanurul@users.noreply.github.com>
2021-04-30 11:03:19 +0700

    Update acak.php

--- praktikum6/ganca/acak.php
+++ praktikum6/ganca/acak.php
@@ -23,8 +23,7 @@
             </form>
         <?php
         } else {
-            echo "Selamat yaaa.. Anda Benar, saya telah memilih bilangan ".$acak;
-            echo "<p><a href='masuk.php'>Coba Lagi</a></p>";
+            Header(Location : keluar.php);
         }
 
     }
