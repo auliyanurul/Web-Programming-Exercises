105752524adb1c5f272a8961aa47d1459e6167ca
auliyanurul <58121850+auliyanurul@users.noreply.github.com>
2021-05-10 22:04:58 +0700

    Add files via upload

---
+++ UTS/config.php
@@ -0,0 +1,9 @@
+<?php 
+$databaseHost = 'unaux_28124168';
+$databaseName = 'sql213.unaux.com';
+$databaseUsername = 'unaux_28124168_gamemath';
+$databasePassword = '176pv32b7ay9jb';
+
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,$databasePassword, $databaseName) or die(mysqli_connect_error()); 
+?>
