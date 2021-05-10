0a4d10caf40a197523e294b752e33a65a034a85e
auliyanurul <58121850+auliyanurul@users.noreply.github.com>
2021-05-10 22:41:10 +0700

    Add files via upload
    Demonstrasi:
    http://pemweb19.unaux.com

---
+++ UTS/config.php
@@ -0,0 +1,9 @@
+<?php 
+$databaseHost = 'sql213.unaux.com';
+$databaseName =  'unaux_28124168_gamemath';
+$databaseUsername = 'unaux_28124168';
+$databasePassword = '176pv32b7ay9jb';
+
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,$databasePassword, $databaseName) or die(mysqli_connect_error()); 
+?>
